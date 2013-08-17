f8bf7df3b2c7a0b679fc744b05bd4608e49085a4
cmeo226 <cmeo226@yahoo.com>
2013-08-17 18:44:28 +0000

    Corrected buf.toString

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
   var pg = fs.readFileSync('index.html');
-  var buf = new buffer(pg);
-  response.send(buf);
+  var buf = new Buffer(pg);
+  response.send(buf.toString);
 });
 
 var port = process.env.port || 5000;
