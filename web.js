6883ab6ba00f4e5e95d196ca556b5cf22177bfe4
cmeo226 <cmeo226@yahoo.com>
2013-08-17 20:43:24 +0000

    testing something

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("lk");
+  response.send('Hello World!');
 });
 
-var port = process.env.port || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
