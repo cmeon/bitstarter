514b836839b1f76c59aff69fbbeff105df190854
cmeo226 <cmeo226@yahoo.com>
2013-08-17 18:38:49 +0000

    Hello World from index.html

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var pg = fs.readFileSync('index.html');
+  var buf = new buffer(pg);
+  response.send(buf);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.port || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
